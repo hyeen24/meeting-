baa7e83ebac925b40be92adbc4bb897d8c9f3a36
Hyeen <81229303+hyeen24@users.noreply.github.com>
2025-03-09 20:48:21 +0800

    Added Background image and main page

---
+++ MySecretary/app/(tabs)/index.jsx
@@ -0,0 +1,40 @@
+import { View, Text , StyleSheet, ImageBackground} from 'react-native'
+import React from 'react'
+import meetingRoomImg from "@/assets/images/meeting_room.png";
+
+const index = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={meetingRoomImg}
+        style={styles.image}>
+
+      <Text style={styles.text}>Meeting Room</Text>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  text : {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  }
+
+})
